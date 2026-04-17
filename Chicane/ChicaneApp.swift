441657cb39d0a2cbe4f1a1ce36ec12b09f8b0ba0
Don Noel <donnoel@icloud.com>
2026-04-17 10:28:10 -0700

    Add Settings version row backed by bundle version/build

--- Chicane/ChicaneApp.swift
+++ Chicane/ChicaneApp.swift
@@ -6,6 +6,8 @@ struct ChicaneApp: App {
     @StateObject private var viewModel: AppViewModel
 
     init() {
+        Self.updateSettingsVersionDisplay()
+
         let bundledDrivers = BundledDriverRepository()
         let bundledCalendar = BundledCalendarRepository()
         let onlineDrivers = OnlineDriverRepository()
@@ -37,5 +39,23 @@ struct ChicaneApp: App {
             ContentView()
                 .environmentObject(viewModel)
         }
+    }
+    private static func updateSettingsVersionDisplay() {
+        let info = Bundle.main.infoDictionary
+        let version = info?["CFBundleShortVersionString"] as? String
+        let build = info?["CFBundleVersion"] as? String
+
+        let displayValue: String
+        if let version, !version.isEmpty {
+            if let build, !build.isEmpty {
+                displayValue = "\(version) (\(build))"
+            } else {
+                displayValue = version
+            }
+        } else {
+            displayValue = "--"
+        }
+
+        UserDefaults.standard.set(displayValue, forKey: "app_version_display")
     }
 }
